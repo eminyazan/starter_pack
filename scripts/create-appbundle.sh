f08ed178197f3f338453b4c736690cf0e14083cf
eminyazan <eminyazan12@gmail.com>
2023-12-19 17:29:54 +0300

    app bundle script updated

--- scripts/create-appbundle.sh
+++ scripts/create-appbundle.sh
@@ -1,7 +1,19 @@
 flutter clean
+
 flutter build appbundle --release --no-tree-shake-icons
+
 cd ..
+
 # shellcheck disable=SC2164
 cd "$(pwd)/build/app/outputs/bundle/release/"
-mv app-release.aab Tripy.aab
+
+# shellcheck disable=SC2034
+current_date=$(date "+%d %b")
+
+# shellcheck disable=SC2034
+app_name="APP_NAME"
+
+# shellcheck disable=SC2034
+mv app-release.aab "${app_name} ${current_date}.aab"
+
 open .
